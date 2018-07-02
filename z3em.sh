df9c58c15aa1e1e5c135d430f45f7ee470769198
Simmo Saan <simmo.saan@gmail.com>
2018-07-02 14:03:03 +0300

    Add sed hack to expose onReceiveInstance callback instead of wasmModule

--- z3em.sh
+++ z3em.sh
@@ -10,4 +10,4 @@ emmake make -C build -j4
 emcc -s INVOKE_RUN=0 -O3 -s MODULARIZE=1 -s EXPORT_NAME="'Z3Em'" -s STRICT=1 -s ERROR_ON_UNDEFINED_SYMBOLS=1 -s DISABLE_EXCEPTION_CATCHING=0 -s ABORTING_MALLOC=0 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS='["_Z3_mk_config", "_Z3_mk_context", "_Z3_mk_config", "_Z3_del_config", "_Z3_eval_smtlib2_string", "_Z3_set_error_handler", "_Z3_global_param_set", "_Z3_del_context"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "addFunction", "removeFunction"]' -s RESERVED_FUNCTION_POINTERS=1 -fPIC -s WASM=1 -s BINARYEN_ASYNC_COMPILATION=1 build/libz3.a -o ../z3em.js
 
 # receiveInstance(instance,module){Module["wasmModule"]=module;
-sed -i -r 's/(receiveInstance\(instance,module\)\{)/\1Module["wasmModule"]=module;/g' ../z3em.js
+sed -i -r 's/(receiveInstance\(instance,module\)\{)/\1if(Module["onReceiveInstance"]){Module["onReceiveInstance"](instance,module)}/g' ../z3em.js
